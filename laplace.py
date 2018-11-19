cbb03bf27dd879be983f024b49aba0181981caa4
akhil <akhil4400@gmail.com>
2018-11-19 16:13:08 -0600

    fix white space, and gitignore

--- laplace.py
+++ laplace.py
@@ -6,29 +6,29 @@ class Grid:
     """
         Simple class to generate a computational grid and apply boundary conditions.
     """
-    
+
     def __init__(self, nx=10, ny=10, xmin=0.0, xmax=1.0, ymin=0.0, ymax=1.0):
-        
+
         self.xmin, self.xmax, self.ymin, self.ymax = xmin, xmax, ymin, ymax
         self.nx, self.ny = nx, ny
-        
+
         self.dx = (xmax - xmin) / (nx - 1)
-        
+
         self.dy = (ymax - ymin) / (ny - 1)
-        
+
         self.u = np.zeros((ny, nx), dtype=np.double)
 
 
     def set_boundary_condition(self, side, boundary_condition_function = lambda x,y: 0.0):
-    
+
         xmin, ymin = self.xmin, self.ymin
-        
+
         xmax, ymax = self.xmax, self.ymax
-        
+
         x = np.arange(xmin, xmax, self.dx)
-        
+
         y = np.arange(ymin, ymax, self.dy)
-        
+
         if side == 'bottom':
             self.u[0 ,:] = boundary_condition_function(xmin, y)
         elif side == 'top':
@@ -37,55 +37,55 @@ class Grid:
             self.u[:, 0] = boundary_condition_function(x, ymin)
         elif side == 'right':
             self.u[:, -1] = boundary_condition_function(x, ymax)
-        
+
 
 class LaplaceSolver(Grid):
     """
-        Class that solves the Laplace equation in 2D 
+        Class that solves the Laplace equation in 2D
     """
-    
+
     def iterate(self):
         """
             A Python (slow) implementation of a finite difference iteration
         """
-        
+
         u = self.u
-        
-        nx, ny = u.shape        
-        
+
+        nx, ny = u.shape
+
         dx2, dy2 = self.dx ** 2, self.dy ** 2
-        
+
         err = 0.0
-        
+
         for i in range(1, nx - 1):
-            
+
             for j in range(1, ny - 1):
-                
+
                 tmp = u[i,j]
-                
+
                 u[i,j] = ((u[i-1, j] + u[i+1, j]) * dy2 +
                           (u[i, j-1] + u[i, j+1]) * dx2) / (dx2 + dy2) / 2
-                
+
                 diff = u[i,j] - tmp
-                
+
                 err += diff * diff
 
         return np.sqrt(err)
-    
-                
-    def solve(self, max_iterations=10000, tolerance=1.0e-16, quiet=False):        
+
+
+    def solve(self, max_iterations=10000, tolerance=1.0e-16, quiet=False):
         """
             Calls iterate() sequentially until the error is reduced below a tolerance.
         """
-        
+
         for i in range(max_iterations):
-        
+
             error = self.iterate()
-            
+
             if error < tolerance:
                 if not quiet:
                     print("Solution converged in " + str(i) + " iterations.")
                 break
-                
+
     def get_solution(self):
         return self.u
--- project2.py
+++ project2.py
@@ -6,37 +6,37 @@ from laplace import LaplaceSolver
 
 
 class LaplaceSolverMPI(LaplaceSolver):
-    
+
     def __init__(self, comm, **kwargs):
         self.comm = comm
         self.rank = comm.rank
         self.size = comm.size
         super().__init__(**kwargs)
-            
-        
+
+
     def set_boundary_condition(self, **kwargs):
         if self.rank == 0:
             super().set_boundary_condition(**kwargs)
-        
-        
-        
+
+
+
     def solve(self, max_iterations=10000, tolerence=1.0e-16, verbose=False):
-        
-        return 
-        
+
+        return
+
 
 
     def get_solution(self):
 
-        return 
+        return
 
-        
+
 if __name__ == "__main__":
-    
+
     from mpi4py import MPI
-    
+
     comm = MPI.COMM_WORLD
-    
+
     solver = LaplaceSolverMPI(comm, nx=4, ny=3)
     solver.set_boundary_condition(side='right', boundary_condition_function=lambda x,y: 5)
     solver.solve(verbose=True)
