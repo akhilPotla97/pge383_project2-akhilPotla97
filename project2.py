4acc4233a79b0ed31c4f776b059dbb0ebef4d8f2
akhil <akhil4400@gmail.com>
2018-11-19 19:55:24 -0600

    well here we go

--- project2.py
+++ project2.py
@@ -11,6 +11,9 @@ class LaplaceSolverMPI(LaplaceSolver):
         self.comm = comm
         self.rank = comm.rank
         self.size = comm.size
+        # print('=======================')
+        # print('rank: ', self.rank)
+        # print('size: ', self.size)
         super().__init__(**kwargs)
 
 
@@ -20,15 +23,24 @@ class LaplaceSolverMPI(LaplaceSolver):
 
 
 
-    def solve(self, max_iterations=10000, tolerence=1.0e-16, verbose=False):
-
-        return
-
-
-
-    def get_solution(self):
-
-        return
+#    def solve(self, max_iterations=10000, tolerence=1.0e-16, verbose=False):
+#        """
+#        1. distribute the rows of the solution matrix as evenly as possible
+#        amoung the processers
+#        """
+#        n = np.array_split(self.u, self.size)
+#        print('u: ', self.u)
+#        print('n: ', n)
+#        print('')
+#        print('')
+#
+#        return
+#
+#
+#
+#    def get_solution(self):
+#
+#        return
 
 
 if __name__ == "__main__":
@@ -38,6 +50,7 @@ if __name__ == "__main__":
     comm = MPI.COMM_WORLD
 
     solver = LaplaceSolverMPI(comm, nx=4, ny=3)
+    print(solver.u)
     solver.set_boundary_condition(side='right', boundary_condition_function=lambda x,y: 5)
     solver.solve(verbose=True)
     u = solver.get_solution()
